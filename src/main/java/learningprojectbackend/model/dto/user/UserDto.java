b2ff80215de51f24fa71b35976dd064c6173e3ca
Szalay Tamás <szalaytamas90@gmail.com>
2023-08-06 17:19:40 +0200

    refactored to match frontend

--- src/main/java/learningprojectbackend/model/dto/user/UserDto.java
+++ src/main/java/learningprojectbackend/model/dto/user/UserDto.java
@@ -10,5 +10,8 @@ import lombok.NoArgsConstructor;
 public class UserDto {
     private Long id;
     private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
     private String roles;
 }
