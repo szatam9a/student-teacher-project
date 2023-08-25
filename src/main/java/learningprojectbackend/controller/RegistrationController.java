1b5208917f4d937893c4f9852a3456912134ae87
Szalay Tamás <szalaytamas90@gmail.com>
2023-08-25 17:39:40 +0200

    api prefix moved to the properties file.

--- src/main/java/learningprojectbackend/controller/RegistrationController.java
+++ src/main/java/learningprojectbackend/controller/RegistrationController.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/registration")
+@RequestMapping("registration")
 @RequiredArgsConstructor
 public class RegistrationController {
     private final UserService userService;
