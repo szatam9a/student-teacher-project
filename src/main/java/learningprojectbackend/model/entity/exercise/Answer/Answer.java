9beee16b573eca1b5c908ed7e45eb538b213aee5
Szalay Tamás <szalaytamas90@gmail.com>
2023-09-08 23:01:21 +0200

    answer structure changed

--- src/main/java/learningprojectbackend/model/entity/exercise/Answer/Answer.java
+++ src/main/java/learningprojectbackend/model/entity/exercise/Answer/Answer.java
@@ -5,6 +5,8 @@ import learningprojectbackend.model.entity.exercise.Exercise;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @Getter
@@ -15,4 +17,12 @@ public class Answer {
     private Long id;
     @ManyToOne
     private Exercise exercise;
+    private String sentenceBeginning;
+    private String sentenceEnding;
+    private List<String> correctAnswers;
+    private int position;
+    private String answer;
+    private boolean isCorrect;
+    private String leftPair;
+    private String rightPair;
 }
