5a0733cb315c0b61183d268e426156d517e86841
Szalay Tamás <szalaytamas90@gmail.com>
2023-09-16 20:01:33 +0200

    endpoint path fixed

--- src/main/java/learningprojectbackend/studies/controller/RegistrationController.java
+++ src/main/java/learningprojectbackend/studies/controller/RegistrationController.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("registration")
+@RequestMapping("/register")
 @RequiredArgsConstructor
 public class RegistrationController {
     private final UserService userService;
