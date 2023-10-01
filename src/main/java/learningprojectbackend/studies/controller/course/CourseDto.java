e2c733ac6f7570f48c71314516242e93f5a727d1
Szalay Tamás <szalaytamas90@gmail.com>
2023-10-01 16:11:53 +0200

    small fix for unmapped properties on frontend

--- src/main/java/learningprojectbackend/studies/controller/course/CourseDto.java
+++ src/main/java/learningprojectbackend/studies/controller/course/CourseDto.java
@@ -3,13 +3,17 @@ package learningprojectbackend.studies.controller.course;
 import learningprojectbackend.studies.service.entity.course.Lesson;
 import learningprojectbackend.studies.service.entity.tag.Tag;
 import lombok.Getter;
+import lombok.Setter;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Getter
+@Setter
 public class CourseDto {
     private Long id;
     private String title;
-    private List<Lesson> lessons;
-    private List<Tag> tags;
+    private List<Lesson> lessons = new LinkedList<>();
+    private List<Tag> tags = new LinkedList<>();
+
 }
--- src/main/java/learningprojectbackend/studies/controller/course/LessonDto.java
+++ src/main/java/learningprojectbackend/studies/controller/course/LessonDto.java
@@ -3,13 +3,16 @@ package learningprojectbackend.studies.controller.course;
 import learningprojectbackend.studies.service.entity.studerial.Studerial;
 import learningprojectbackend.studies.service.entity.tag.Tag;
 import lombok.Getter;
+import lombok.Setter;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Getter
+@Setter
 public class LessonDto {
     private Long id;
     private String title;
-    private List<Studerial> studerials;
-    private List<Tag> tags;
+    private List<Studerial> studerials = new LinkedList<>();
+    private List<Tag> tags = new LinkedList<>();
 }
