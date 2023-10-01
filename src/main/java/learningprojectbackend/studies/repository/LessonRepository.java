fc2e6879fb7bd9408279f168faa071e2faee9ce1
Szalay Tamás <szalaytamas90@gmail.com>
2023-10-01 20:15:45 +0200

    get lessons and tags for course update

--- src/main/java/learningprojectbackend/studies/repository/LessonRepository.java
+++ src/main/java/learningprojectbackend/studies/repository/LessonRepository.java
@@ -2,6 +2,14 @@ package learningprojectbackend.studies.repository;
 
 import learningprojectbackend.studies.service.entity.course.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
+
+
+    @Query("select l from Lesson l where l.id in :ids and l.user.id = :userId")
+    List<Lesson> findAllByIdInAndUserId(@Param("ids") List<Long> ids, @Param("userId") Long userId);
 }
--- src/main/java/learningprojectbackend/studies/repository/TagRepository.java
+++ src/main/java/learningprojectbackend/studies/repository/TagRepository.java
@@ -2,6 +2,8 @@ package learningprojectbackend.studies.repository;
 
 import learningprojectbackend.studies.service.entity.tag.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,5 +11,9 @@ public interface TagRepository extends JpaRepository<Tag, Long> {
 
     //@Query("select t from Tag t where t.user.id = ?1")
     List<Tag> findAllByUserId(Long userId);
+
+
+    @Query("select t from Tag t where t.id in :ids and t.user.id = :userId")
+    List<Tag> findAllByIdInAndUserId(@Param("ids") List<Long> ids, @Param("userId") Long userId);
 
 }
