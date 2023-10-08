93119b107949421b564893758adee88b42ddfe5d
Szalay Tamás <szalaytamas90@gmail.com>
2023-10-08 19:55:11 +0200

    onetomany changed to manytomany relationship,  sets  introduced instead of lists

---
+++ src/main/java/learningprojectbackend/studies/controller/course/CourseFilterDto.java
@@ -0,0 +1,12 @@
+package learningprojectbackend.studies.controller.course;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CourseFilterDto {
+    private Long id;
+    private String title;
+    private int lessonsNumber;
+}
